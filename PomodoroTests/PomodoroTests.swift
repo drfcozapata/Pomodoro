04b335e02272b5fe11608bd20836b8aa0c7d0883
Francisco Zapata <drfcozapata@gmail.com>
2026-04-14 08:54:44 -0400

    Initial Commit

---
+++ PomodoroTests/PomodoroTests.swift
@@ -0,0 +1,19 @@
+//
+//  PomodoroTests.swift
+//  PomodoroTests
+//
+//  Created by Francisco Zapata on 14/4/26.
+//
+
+import Testing
+@testable import Pomodoro
+
+struct PomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
